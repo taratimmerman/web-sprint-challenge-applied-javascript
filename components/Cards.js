27bc8db61fe1a4c63665e3d04f88903b08b66f40
taratimmerman <timmermantara@gmail.com>
2020-12-04 11:31:36 -0700

    30 minute warning commit, cards broken, node issue

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,59 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+axios.get("https://lambda-times-api.herokuapp.com/articles")
+    .then((res) => {
+        const articles = res.data.articles;
+        console.log(articles);
+        const articleTopics = Object.keys(articles);
+        console.log(articleTopics);
+        articleTopics.forEach(i => {
+            const topic = articles[i];
+            topic.forEach(j => {
+                const newCard = articleMaker(j);
+                cardContainer.appendChild(newCard);
+            });
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+function articleMaker(article) {
+    
+    // Establish elements
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authName = document.createElement('span');
+
+    // Assign class names
+    card.classList.add('card');
+    headline.classList.add('headline');
+    authDiv.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    // Establish textContent & src
+    headline.textContent = `${article.headline}`;
+    img.src = article.authorPhoto;
+    authName.textContent = `By ${article.authorName}`;
+
+    // Append
+    card.appendChild(headline);
+    card.appendChild(authDiv);
+    authDiv.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    authDiv.appendChild(authName);
+
+    // Add eventListener
+    card.addEventListener('click', () => {
+        console.log(headline);
+    })
+
+    return article;
+}
