74bf0b763f5ac4180f2de137ef1ce843113d9fb6
taratimmerman <timmermantara@gmail.com>
2020-12-04 10:53:23 -0700

    Tabs Complete

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,31 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+// Variable to append tab to DOM
+const topicsTab = document.querySelector('.topics');
+
+// Data get via axios and append
+axios.get("https://lambda-times-api.herokuapp.com/topics")
+    .then((res) => {
+        const topicArr = res.data.topics;
+        topicArr.forEach(item => {
+            topicsTab.appendChild(topicMaker(item));
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+function topicMaker(item) {
+    // Create Element
+    const topic = document.createElement('div');
+
+    // Assign class name
+    topic.classList.add('tab');
+
+    // Establish textContent
+    topic.textContent = item;
+
+    return topic;
+}
